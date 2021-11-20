deee19ec2fd317cc4ed042630c95d4c2905b9c45
David Rachwalik <david.rachwalik@outlook.com>
2021-11-20 10:30:43 -0600

    Add paging

--- ContosoUniversity/Pages/Students/Index.cshtml.cs
+++ ContosoUniversity/Pages/Students/Index.cshtml.cs
@@ -1,25 +1,23 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
 using ContosoUniversity.Data;
 using ContosoUniversity.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace ContosoUniversity.Pages.Students
 {
     public class IndexModel : PageModel
     {
         private readonly SchoolContext _context;
-        private readonly MvcOptions _mvcOptions;
+        private readonly IConfiguration Configuration;
 
-        public IndexModel(SchoolContext context, IOptions<MvcOptions> mvcOptions)
+        public IndexModel(SchoolContext context, IConfiguration configuration)
         {
             _context = context;
-            _mvcOptions = mvcOptions.Value;
+            Configuration = configuration;
         }
 
         public string NameSort { get; set; }
@@ -27,12 +25,22 @@ namespace ContosoUniversity.Pages.Students
         public string CurrentFilter { get; set; }
         public string CurrentSort { get; set; }
 
-        public IList<Student> Students { get; set; }
+        public PaginatedList<Student> Students { get; set; }
 
-        public async Task OnGetAsync(string sortOrder, string searchString)
+        public async Task OnGetAsync(string sortOrder,
+            string currentFilter, string searchString, int? pageIndex)
         {
+            CurrentSort = sortOrder;
             NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
             DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
 
             CurrentFilter = searchString;
 
@@ -43,7 +51,6 @@ namespace ContosoUniversity.Pages.Students
                 studentsIQ = studentsIQ.Where(s => s.LastName.Contains(searchString)
                                        || s.FirstMidName.Contains(searchString));
             }
-
             switch (sortOrder)
             {
                 case "name_desc":
@@ -59,10 +66,10 @@ namespace ContosoUniversity.Pages.Students
                     studentsIQ = studentsIQ.OrderBy(s => s.LastName);
                     break;
             }
-            // MaxModelBindingCollectionSize defaults to 1024
-            studentsIQ = studentsIQ.Take(_mvcOptions.MaxModelBindingCollectionSize);
 
-            Students = await studentsIQ.AsNoTracking().ToListAsync();
+            var pageSize = Configuration.GetValue("PageSize", 4);
+            Students = await PaginatedList<Student>.CreateAsync(
+                studentsIQ.AsNoTracking(), pageIndex ?? 1, pageSize);
         }
     }
 }
