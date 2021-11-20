f5e1ef80dba9a86941bcab14f8e3b2360fbd9e73
David Rachwalik <david.rachwalik@outlook.com>
2021-11-20 07:52:16 -0600

    add sorting

--- ContosoUniversity/Pages/Students/Index.cshtml.cs
+++ ContosoUniversity/Pages/Students/Index.cshtml.cs
@@ -22,13 +22,63 @@ namespace ContosoUniversity.Pages.Students
             _mvcOptions = mvcOptions.Value;
         }
 
-        public IList<Student> Student { get;set; }
+        public string NameSort { get; set; }
+        public string DateSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
 
-        public async Task OnGetAsync()
+        public IList<Student> Students { get; set; }
+
+        public async Task OnGetAsync(string sortOrder)
         {
+            // using System;
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+
+            IQueryable<Student> studentsIQ = from s in _context.Students
+                                             select s;
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    studentsIQ = studentsIQ.OrderByDescending(s => s.LastName);
+                    break;
+                case "Date":
+                    studentsIQ = studentsIQ.OrderBy(s => s.EnrollmentDate);
+                    break;
+                case "date_desc":
+                    studentsIQ = studentsIQ.OrderByDescending(s => s.EnrollmentDate);
+                    break;
+                default:
+                    studentsIQ = studentsIQ.OrderBy(s => s.LastName);
+                    break;
+            }
             // MaxModelBindingCollectionSize defaults to 1024
-            Student = await _context.Students.Take(
-                _mvcOptions.MaxModelBindingCollectionSize).ToListAsync();
+            studentsIQ = studentsIQ.Take(_mvcOptions.MaxModelBindingCollectionSize);
+
+            Students = await studentsIQ.AsNoTracking().ToListAsync();
         }
     }
+
+
+    //public class IndexModel : PageModel
+    //{
+    //    private readonly SchoolContext _context;
+    //    private readonly MvcOptions _mvcOptions;
+
+    //    public IndexModel(SchoolContext context, IOptions<MvcOptions> mvcOptions)
+    //    {
+    //        _context = context;
+    //        _mvcOptions = mvcOptions.Value;
+    //    }
+
+    //    public IList<Student> Student { get;set; }
+
+    //    public async Task OnGetAsync()
+    //    {
+    //        // MaxModelBindingCollectionSize defaults to 1024
+    //        Student = await _context.Students.Take(
+    //            _mvcOptions.MaxModelBindingCollectionSize).ToListAsync();
+    //    }
+    //}
 }
