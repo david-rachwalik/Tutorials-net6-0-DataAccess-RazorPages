dbe8756ebf8eba88ab04b5736bc326adf47cf551
David Rachwalik <david.rachwalik@outlook.com>
2021-11-21 05:24:14 -0600

    Added migrations

--- ContosoUniversity/Program.cs
+++ ContosoUniversity/Program.cs
@@ -34,7 +34,7 @@ using (var scope = app.Services.CreateScope())
     var services = scope.ServiceProvider;
 
     var context = services.GetRequiredService<SchoolContext>();
-    context.Database.EnsureCreated();
+    //context.Database.EnsureCreated();
     DbInitializer.Initialize(context);
 }
 
